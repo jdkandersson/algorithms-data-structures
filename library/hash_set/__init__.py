f51442a95fbbe2b24dfc3bae24e3d95c7b5f946c
David Andersson <anderssonpublic@gmail.com>
2019-10-05 13:25:00 +1000

    Implemented contains interface

--- library/hash_set/__init__.py
+++ library/hash_set/__init__.py
@@ -17,7 +17,27 @@ class HashSet:
         Add value to the set.
 
         Args:
-            value: The value to add to the set.
+            value: The value to add.
 
         """
         self._hash_map.set_(value, PRESENT)
+
+    def delete(self, value):
+        """
+        Delete value from the set.
+
+        Args:
+            value: The value to delete.
+
+        """
+        self._hash_map.delete(value)
+
+    def contains(self, value):
+        """
+        Check whether the set contains the value.
+
+        Args:
+            value: The value to check for.
+
+        """
+        return self._hash_map.exists(value)
--- tests/test_hash_set.py
+++ tests/test_hash_set.py
@@ -1,5 +1,7 @@
 """Tests for hash set."""
-# pylint: disable=protected-access
+# pylint: disable=protected-access,redefined-outer-name
+
+import pytest
 
 from library import hash_set
 
@@ -17,15 +19,56 @@ def test_construct():
     assert test_hash_set._hash_map._capacity == capacity
 
 
-def test_add():
+@pytest.fixture
+def empty_set():
+    """Return empty HashSet."""
+    return hash_set.HashSet()
+
+
+def test_add(empty_set):
     """
     GIVEN empty hash set and an element
     WHEN add is called with the element
     THEN the element exists in the underlying HashMap.
     """
     element = "element 1"
-    test_hash_set = hash_set.HashSet()
 
-    test_hash_set.add(element)
+    empty_set.add(element)
 
-    assert test_hash_set._hash_map.exists(element)
+    assert empty_set._hash_map.exists(element)
+
+
+@pytest.fixture
+def single_set(empty_set):
+    """Return single item HashSet."""
+    empty_set.add("element 1")
+    return empty_set
+
+
+def test_delete(single_set):
+    """
+    GIVEN HashSet with single element
+    WHEN delete is called with the element
+    THEN the element is no longer in the underlying HashMap.
+    """
+    element = "element 1"
+
+    single_set.delete(element)
+
+    assert not single_set._hash_map.exists(element)
+
+
+@pytest.mark.parametrize(
+    "element, expected_result",
+    [("element 1", True), ("element 2", False)],
+    ids=["in set", "not in set"],
+)
+def test_contains(element, expected_result, single_set):
+    """
+    GIVEN HashSet with single element, element to check for and expected result
+    WHEN contains is called with the element
+    THEN the expected result is returned.
+    """
+    result = single_set.contains(element)
+
+    assert result == expected_result
